2ab972d82eafbffc360c76ce6529804a3665f576
partizan1317 <papa.b.gorode@gmail.com>
2023-04-24 11:49:30 +0300

    Initial commit

---
+++ src/main/java/com/sobol/studentGroup/StudentGroupApplication.java
@@ -0,0 +1,13 @@
+package com.sobol.studentGroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentGroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sobol/studentGroup/StudentGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sobol.studentGroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
